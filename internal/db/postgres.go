53719150deadc8a3d3278106c03a95572283919e
MeIsAkhoa <lekhoa4856@gmail.com>
2026-01-07 14:56:49 +0700

    Add database connection logging

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -9,6 +10,20 @@ import (
 )
 
 func NewPostgres(dbURL string) *sqlx.DB {
+	// Log connection info (without password) for debugging
+	if dbURL != "" {
+		// Mask password in log
+		maskedURL := dbURL
+		if idx := strings.Index(dbURL, "@"); idx > 0 {
+			if passIdx := strings.LastIndex(dbURL[:idx], ":"); passIdx > 0 {
+				maskedURL = dbURL[:passIdx+1] + "***" + dbURL[idx:]
+			}
+		}
+		log.Printf("Connecting to database: %s", maskedURL)
+	} else {
+		log.Println("WARNING: DATABASE_URL is empty!")
+	}
+	
 	db, err := sqlx.Connect("postgres", dbURL)
 	if err != nil {
 		log.Fatalf("failed to connect DB: %v", err)
